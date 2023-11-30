724aad013dfc93b929ae2d736f010a92de0146f8
hafizakhanam <hafiza.khanam0@gmail.com>
2023-11-30 18:30:40 +0600

    reviews api

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const stripe =require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+      'https://wedding-door.web.app',
+      'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -26,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
 
     const userCollection = client.db("weddingDB").collection("users");
@@ -142,7 +148,7 @@ async function run() {
     })
 
     //Bio Data collection
-    app.get('/bioData/all', verifyToken, verifyAdmin, async(req, res) =>{
+    app.get('/bioData/all', async(req, res) =>{
       const result = await bioDataCollection.find().toArray();
       res.send(result);
     })
@@ -249,19 +255,12 @@ async function run() {
       res.send(result);
     })
 
-    // app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) =>{
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)};
-    //   const result = await menuCollection.deleteOne(query);
-    //   res.send(result);
-    // })
-
     app.get('/reviews', async(req, res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
     })
 
-    app.post('/reqPremium', async(req, res) =>{
+    app.post('/reqPremium', verifyToken, async(req, res) =>{
       const reqItem = req.body;
       const result = await reqPremiumCollection.insertOne(reqItem);
       res.send(result);
@@ -315,7 +314,7 @@ async function run() {
       res.send(result);
     })
 
-    app.post('/reqContacts', async(req, res) =>{
+    app.post('/reqContacts', verifyToken, async(req, res) =>{
       const payment = req.body;
       if(payment){
         const paymentResult = await reqContactCollection.insertOne(payment);
@@ -354,11 +353,16 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/reviews', async(req, res) =>{
+      const review = req.body;
+      //console.log(newFood)
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    })
     // stats or analytics
     app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res) =>{
       const users = await userCollection.estimatedDocumentCount();
       const bioDataItems = await bioDataCollection.estimatedDocumentCount();
-      const orders = await paymentCollection.estimatedDocumentCount();
 
       const pipeline = [
         {
@@ -394,14 +398,13 @@ async function run() {
         bioDataItems,
         maleCount,
         femaleCount,
-        orders,
         revenue
       });
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
